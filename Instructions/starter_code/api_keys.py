6f08a17def4cb260c50c4fd97cdf5c1cddf673fc
nowlansavage <nowlansavage@gmail.com>
2020-07-29 22:00:11 -0500

    First commit

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "b6f0a84b2a4486158839dd2d3ba246fe"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCVawYNFCZ6aQrKXfP1mWwoG2txTRtknKQ"
